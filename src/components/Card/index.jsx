3daa9889f7463fe2925f4e89997d20378237e654
leonardo <leonardoorodrigues65@gmail.com>
2023-01-30 13:09:58 -0300

    atualização

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -20,7 +20,7 @@ function Card({transaction, amount, id, type, setList, list, setFilter}){
                 <small className={styles.transactionType}>{type}</small>
             </div>
             <div className={styles.amountButtonContainer}>
-                <span className={styles.amount}>U$ {amount}</span>
+                <span className={styles.amount}>R$ {amount}</span>
                 <button className={styles.deleteButton} onClick={() => {deleteTransaction(id)}}><MdDelete/></button>
             </div>
         
